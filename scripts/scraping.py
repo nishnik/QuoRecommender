584d8a995ba6a46d8904b26b067859c6ca9f7d80
Abhinav Jain <abhinavjain241@gmail.com>
2016-03-25 11:03:56 +0530

    Remove recurring instances of the same tag | More formatting changes

--- scripts/scraping.py
+++ scripts/scraping.py
@@ -1,34 +1,36 @@
-# To run in DEBUG mode, add -O flag, i.e python -O scraping.py
+# To run without DEBUG mode, add -O flag, i.e python -O scraping.py
 import os,json
 import requests
 from bs4 import BeautifulSoup
-dic={}
-count=0
+
+dic = {}
+count = 0
 
 PATH = "../logs/"
 for i in os.listdir(PATH):
 	filename = PATH + i
 	soup = BeautifulSoup(open(filename))
-	question = soup.find_all("div", {"class":"revision"})
+	question = soup.find_all("div", {"class" : "revision"})
 	try:
 		q = question[-1]
 		if __debug__:
 			print q.getText()
 		q = q.getText()
 		q = q.encode('utf-8')
-		print q
-		topic = soup.find_all("span",{"class":"TopicName"})
+		topic = soup.find_all("span", {"class" : "TopicName"})
 		topics = []
 		for i in topic:
-			topics.append((i.getText()).encode('utf-8'))
+			topicName = (i.getText()).encode('utf-8')
+			if not topicName in topics:
+				topics.append(topicName)
 		if __debug__:
 			print topics
 		try:
-			dic[q]=topics
+			dic[q] = topics
 		except UnicodeEncodeError:
 			print "Can't write"
 	except IndexError:
 		pass
-filehandle = open("tags.json","w")
-json.dump(dic,filehandle,indent = 2)
+filehandle = open("tags.json", "w")
+json.dump(dic, filehandle, indent = 2)
 filehandle.close()
