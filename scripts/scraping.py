8ea1447225a47d6ad523d6fe2dbd70779e2dc5c8
Abhinav Jain <abhinavjain241@gmail.com>
2016-03-25 10:49:19 +0530

    Made formatting changes | Added Debug Mode | Output into JSON

--- scripts/scraping.py
+++ scripts/scraping.py
@@ -1,30 +1,34 @@
+# To run in DEBUG mode, add -O flag, i.e python -O scraping.py
 import os,json
 import requests
 from bs4 import BeautifulSoup
 dic={}
 count=0
-for i in os.listdir('~/Documents/DA/Quora_logs'):
-	filename="~/Documents/DA/Quora_logs/"+i
-	#print filename
-	soup=BeautifulSoup(open(filename))
-	question = soup.find_all("div",{"class":"revision"})
+
+PATH = "../logs/"
+for i in os.listdir(PATH):
+	filename = PATH + i
+	soup = BeautifulSoup(open(filename))
+	question = soup.find_all("div", {"class":"revision"})
 	try:
-		q=question[-1]
-		#print q.getText()
-		q=q.getText()
-		q=q.encode('utf-8')
+		q = question[-1]
+		if __debug__:
+			print q.getText()
+		q = q.getText()
+		q = q.encode('utf-8')
 		print q
-		topic=soup.find_all("span",{"class":"TopicName"})
-		topics=[]
+		topic = soup.find_all("span",{"class":"TopicName"})
+		topics = []
 		for i in topic:
 			topics.append((i.getText()).encode('utf-8'))
-		#print topics
+		if __debug__:
+			print topics
 		try:
 			dic[q]=topics
 		except UnicodeEncodeError:
-			print "can't write"
+			print "Can't write"
 	except IndexError:
 		pass
-filehandle=open("text.txt","w")
-json.dump(dic,filehandle,indent=2)
+filehandle = open("tags.json","w")
+json.dump(dic,filehandle,indent = 2)
 filehandle.close()
